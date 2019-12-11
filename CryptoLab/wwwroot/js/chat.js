daf1149575c8ce2beb219a97f0cb795cfe55781c
Dmitriy <dmitriy.kotenko@gmail.com>
2019-12-11 22:38:22 +0200

    Create layout and add functionality

--- CryptoLab/wwwroot/js/chat.js
+++ CryptoLab/wwwroot/js/chat.js
@@ -1,60 +1,71 @@
-﻿"use strict";
+﻿var connection = new signalR.HubConnectionBuilder().withUrl('/chatHub').build();
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var selectedUser;
 
-document.getElementById("sendButton").disabled = true;
+connection.on('ClientConnected', appendUser);
 
-connection.on("ClientConnected", function(userEmail) {
-    var li = document.createElement("a");
-    li.textContent = userEmail;
-    li.id = userEmail;
-    li.className = "list-group-item list-group-item-action";
-    li.setAttribute("data-toggle", "tab");
-    li.setAttribute("role", "tab");
-    document.getElementById("usersList").appendChild(li);
+connection.on('UserList', function (users) {
+    users.forEach(appendUser);
 });
 
-connection.on("UserList", function(users) {
-    users.forEach(user => {
-        var li = document.createElement("a");
-        li.textContent = user;
-        li.id = user;
-        li.className = "list-group-item list-group-item-action";
-        li.setAttribute("data-toggle", "tab");
-        li.setAttribute("role", "tab");
-        document.getElementById("usersList").appendChild(li);
-    });
-});
-
-connection.on("ClientDisconnected", function(userEmail) {
+connection.on('ClientDisconnected', function (userEmail) {
     var userLi = document.getElementById(userEmail);
-    document.getElementById("usersList").removeChild(userLi);
+    document.getElementById('usersList').removeChild(userLi);
 });
 
-connection.on("ReceiveMessage", function(message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var li = document.createElement("li");
-    li.textContent = msg;
-    document.getElementById("messagesList").appendChild(li);
+connection.on('ReceiveMessage', function (message) {
+    appendMessage(selectedUser, message);
 });
 
-connection.start().then(function() {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function(err) {
-    return console.error(err.toString());
-});
+connection.start();
 
-document.getElementById("sendButton").addEventListener("click", function(event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function(err) {
+$('#sendButton').click(e => {
+    var messageInput = $('#messageInput');
+    var message = messageInput.val();
+
+    appendMessage('You', message);
+
+    connection.invoke('SendMessage', selectedUser, message).catch(function (err) {
         return console.error(err.toString());
     });
-    event.preventDefault();
+    messageInput.val('');
+    e.preventDefault();
 });
 
-$('#usersList a').on('click', function(e) {
-    console.log("aa");
+function appendUser(userEmail) {
+    var userItem = document.createElement('a');
+    userItem.textContent = userEmail;
+    userItem.href = '#';
+    userItem.id = userEmail;
+    userItem.className = 'list-group-item list-group-item-action list-group-item-light';
+    userItem.setAttribute('data-toggle', 'tab');
+    userItem.setAttribute('role', 'tab');
+    userItem.onclick = onUserClick;
+    document.getElementById('usersList').appendChild(userItem);
+}
+
+function appendMessage(user, message) {
+    var element = document.createElement('p');
+    element.textContent = user + ': ' + message;
+    document.getElementById('messagesList').appendChild(element);
+}
+
+function onUserClick(e) {
     e.preventDefault();
     $(this).tab('show');
-})
+}
+
+function onUserSelected(e) {
+    selectedUser = e.target.id;
+    document.getElementById('startConversation').disabled = false;
+}
+
+
+$('#startConversation').click(e => {
+    $('#' + e.target.id).prop("disabled", true);
+    $('#conversation').show();
+});
+
+$(document).ready(function () {
+    $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', onUserSelected);
+});
