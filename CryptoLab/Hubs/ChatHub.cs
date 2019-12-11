d0591661a6fcc15f8a57c4162e79281b7917c80d
Dmitriy Kotenko <dmitriy.kotenko@gmail.com>
2019-12-11 19:40:31 +0200

    Add users list

--- CryptoLab/Hubs/ChatHub.cs
+++ CryptoLab/Hubs/ChatHub.cs
@@ -1,16 +1,58 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace CryptoLab.Hubs
 {
     public class ChatHub : Hub
     {
+        public static ConcurrentDictionary<string, List<string>> ConnectedUsers = new ConcurrentDictionary<string, List<string>>();
         private readonly UserManager<IdentityUser> _userManager;
 
         public ChatHub(UserManager<IdentityUser> userManager)
         {
             _userManager = userManager;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            string userName = Context.User.Identity.Name;
+
+            ConnectedUsers.TryGetValue(userName, out List<string> existingUserConnectionIds);
+            if (existingUserConnectionIds == null)
+            {
+                existingUserConnectionIds = new List<string>();
+            }
+            existingUserConnectionIds.Add(Context.ConnectionId);
+            ConnectedUsers.TryAdd(userName, existingUserConnectionIds);
+
+            await Clients.Caller.SendAsync("UserList", ConnectedUsers.Keys);
+            await Clients.Others.SendAsync("ClientConnected", userName);
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            string userName = Context.User.Identity.Name;
+
+            ConnectedUsers.TryGetValue(userName, out List<string> existingUserConnectionIds);
+
+            if (existingUserConnectionIds != null)
+            {
+                existingUserConnectionIds.Remove(Context.ConnectionId);
+
+                if (existingUserConnectionIds.Count == 0)
+                {
+                    ConnectedUsers.TryRemove(userName, out List<string> _);
+                }
+            }
+            await Clients.Others.SendAsync("ClientDisconnected", userName);
+
+            await base.OnDisconnectedAsync(exception);
         }
 
         public async Task SendMessage(string toUser, string message)
--- CryptoLab/wwwroot/js/chat.js
+++ CryptoLab/wwwroot/js/chat.js
@@ -4,24 +4,57 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (message) {
+connection.on("ClientConnected", function(userEmail) {
+    var li = document.createElement("a");
+    li.textContent = userEmail;
+    li.id = userEmail;
+    li.className = "list-group-item list-group-item-action";
+    li.setAttribute("data-toggle", "tab");
+    li.setAttribute("role", "tab");
+    document.getElementById("usersList").appendChild(li);
+});
+
+connection.on("UserList", function(users) {
+    users.forEach(user => {
+        var li = document.createElement("a");
+        li.textContent = user;
+        li.id = user;
+        li.className = "list-group-item list-group-item-action";
+        li.setAttribute("data-toggle", "tab");
+        li.setAttribute("role", "tab");
+        document.getElementById("usersList").appendChild(li);
+    });
+});
+
+connection.on("ClientDisconnected", function(userEmail) {
+    var userLi = document.getElementById(userEmail);
+    document.getElementById("usersList").removeChild(userLi);
+});
+
+connection.on("ReceiveMessage", function(message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var li = document.createElement("li");
     li.textContent = msg;
     document.getElementById("messagesList").appendChild(li);
 });
 
-connection.start().then(function () {
+connection.start().then(function() {
     document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
+}).catch(function(err) {
     return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
+document.getElementById("sendButton").addEventListener("click", function(event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    connection.invoke("SendMessage", user, message).catch(function(err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
+
+$('#usersList a').on('click', function(e) {
+    console.log("aa");
+    e.preventDefault();
+    $(this).tab('show');
+})
