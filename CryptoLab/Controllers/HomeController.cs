e4ab011bb94171c2c20e4bbfd88b1c76d57ca211
Dmitriy Kotenko <dmitriy.kotenko@gmail.com>
2019-12-11 13:43:19 +0200

    Add solution

---
+++ CryptoLab/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CryptoLab.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CryptoLab.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CryptoLab/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CryptoLab.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ChatHub(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task SendMessage(string toUser, string message)
+        {
+            IdentityUser user = await _userManager.FindByEmailAsync(toUser);
+
+            await Clients.User(user.Id).SendAsync("ReceiveMessage", message);
+        }
+    }
+}
